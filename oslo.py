fa221e0f33872d8edba8779557c69dd5e3fe5fb0
ChiralSquid <andrew.liang2001@gmail.com>
2023-01-18 10:28:59 +0000

    Many functionalities deprecated. Task 2e, 2f started.

--- oslo.py
+++ oslo.py
@@ -1,22 +1,22 @@
 import numpy as np
 from tqdm import tqdm
 import warnings
-import cProfile
-from numba.experimental import jitclass
-from numba import int32
-"""Numba is so fast. All hail numba"""
+from numba.experimental import jitclass  # https://numba.readthedocs.io/en/stable/user/jitclass.html
+from numba import int32, int64
 
 
 @jitclass(spec=[
     ('L', int32),
     ('_heights', int32[:]),
-    ('time', int32),  # largest int32 is 2,147,483,647 so time may be an issue if the model is run for too long
+    ('time', int64),  # time may be quite big so int64 is a safe choice
     ("z_threshold", int32[:]),
     ("z", int32[:]),
 ])
 class OsloModel:
     """A class for the Oslo Model"""
     def __init__(self, L: int):
+        """Note that under the numba implementation, creation of multiple models is slow because of the need for
+        random arrays to be looped over."""
         self.L = L
         self._heights = np.zeros(L, dtype=np.int32)
         temp = np.zeros(L, dtype=np.int32)
@@ -27,10 +27,10 @@ class OsloModel:
         self.z = temp2[:-1] - temp2[1:]
         self.time = 0
 
-    # @property
-    # def heights(self):
-    #     return self._heights
-    #
+    @property
+    def heights(self):
+        return self._heights
+
     # @heights.setter
     # def heights(self, value):
     #     if not isinstance(value, np.ndarray):
@@ -46,22 +46,6 @@ class OsloModel:
     #     temp2 = np.append(self._heights, np.array(0, dtype=np.int32))
     #     self.z = temp2[:-1] - temp2[1:]
 
-    # def __str__(self):
-    #     max_height = np.max(self.heights)
-    #     if max_height >= 20 or self.L >= 20:
-    #         return self.__repr__()
-    #     elif max_height == 0:
-    #         return str(np.zeros((1, self.L), dtype=int))
-    #     else:
-    #         matrix = np.zeros((max_height, self.L), dtype=int)
-    #         for i in range(self.L):
-    #             if self.heights[i] > 0:
-    #                 matrix[-self.heights[i]:, i] = 1
-    #         return f"The Oslo Model looks like:\n{str(matrix)}"
-    #
-    # def __repr__(self):
-    #     return f"The z vector is {self.z}\nThe heights are {self.heights}"
-
     def is_system_stable(self) -> bool:
         """Check if the system is stable by an elementwise comparison of the z and z_threshold vectors.
         It is O(L) and may not always be the most efficient"""
@@ -74,8 +58,6 @@ class OsloModel:
         """Run the model for 1 iteration by placing a grain on the leftmost site."""
         self._heights[0] += 1
         self.z[0] += 1
-
-        # is_stable = self.is_system_stable()
 
         while not self.is_system_stable():
             """This loops brings the system to the next stable state. z and heights are hardcoded independently for
@@ -106,14 +88,15 @@ class OsloModel:
         """Return the indices of the unstable sites"""
         return np.where(self.z > self.z_threshold)[0]
 
-    def doctor(self):
-        """Perform a few standard checks of common issues within the instance of OsloModel"""
-        if not self.is_system_stable():
-            warnings.warn("Current configuration is not stable")
-        elif self.z != -np.diff(np.append(self._heights, 0)):
-            warnings.warn("z and heights are not consistent. Check the run method")
-        else:
-            "No issues found"
+    # def doctor(self):
+    #     """Perform a few standard checks of common issues within the instance of OsloModel"""
+    #     temp2 = np.append(self._heights, np.array(0, dtype=np.int32))
+    #     if not self.is_system_stable():
+    #         print("Current configuration is not stable")
+    #     elif not np.all(self.z, -np.diff(temp2)):
+    #         print("z and heights are not consistent. Check the run method")
+    #     else:
+    #         "No issues found"
 
 
 if __name__ == "__main__":
--- task2.py
+++ task2.py
@@ -11,13 +11,13 @@ anyway. I am not using eq (3) to calculate the height at the leftmost site.
 """
 
 plt.figure()
-for system_size in [4, 8, 16, 32, 64, 128, 256]:
+for system_size in [4, 8, 16, 32, 64, 128, 256, 512]:
     model = OsloModel(system_size)
     result = []
-    for t in tqdm(range(80000)):
+    for t in tqdm(range(250000)):
         model.run()
-        result.append(model._heights[0])
-    plt.plot(np.arange(80000), result, label=f"L={system_size}")
+        result.append(model.heights[0])
+    plt.plot(np.arange(250000), result, label=f"L={system_size}")
 
 plt.legend()
 plt.xlabel("Time t")
@@ -34,9 +34,9 @@ result2ba = []
 for _ in tqdm(range(1000)):  # 1000 runs
     model2b.reset()
     prev_total_height = 0
-    while sum(model2b._heights) == prev_total_height:
+    while sum(model2b.heights) == prev_total_height:
         model2b.run()
-        total_height = sum(model2b._heights)
+        total_height = sum(model2b.heights)
         prev_total_height += 1
     result2ba.append(model2b.time)
 
@@ -46,20 +46,19 @@ plt.ylabel("Count")
 plt.show()
 
 #%% task2b for varying L
-"""This takes a while to run"""
 plt.figure()
 
 result2b = []
 system_size2b = [4, 8, 16, 32, 64, 128, 256]
 
-for sys_size in system_size2b:
+for sys_size in tqdm(system_size2b):
     model = OsloModel(sys_size)
     individual_time = []
-    for _ in tqdm(range(10)):  # 10 runs to compute the average
+    for _ in range(10):  # 10 runs to compute the average
         prev_total_height = 0
-        while sum(model._heights) == prev_total_height:
+        while sum(model.heights) == prev_total_height:
             model.run()
-            total_height = sum(model._heights)
+            total_height = sum(model.heights)
             prev_total_height += 1
         individual_time.append(model.time)
     result2b.append(np.mean(individual_time))
@@ -74,15 +73,83 @@ plt.figure()
 M = 10  # M is the iterations to average over
 
 for system_size in [4, 8, 16, 32]:
+    average = []
     for _ in range(M):
         model = OsloModel(system_size)
-        result = []
+        internal_result = []
         for t in tqdm(range(80000)):
             model.run()
-            result.append(model._heights[0])
-        plt.plot(np.arange(80000), result, label=f"L={system_size}")
+            internal_result.append(model.heights[0])
+        average.append(internal_result)
+
+    plt.plot(np.arange(80000), result, label=f"L={system_size}")
 
 plt.legend()
 plt.xlabel("Time t")
-plt.ylabel("Height at leftmost site h(t; L)")
+plt.ylabel("Height of pile h(t; L)")
+plt.show()
+
+# %%
+"""To estimate t_c for each L. This is taken from the script for 2a"""
+
+plt.figure()
+for system_size in [4, 8, 16, 32, 64, 128]:
+    model = OsloModel(system_size)
+    result = []
+    for t in tqdm(range(25000)):
+        model.run()
+        result.append(model.heights[0])
+    plt.plot(np.arange(25000), result, label=f"L={system_size}")
+
+plt.legend()
+plt.xlabel("Time t")
+plt.ylabel("Height of the pile h(t; L)")
+plt.show()
+
+
+#%% task 2e, 2f
+plt.figure()
+
+T = 200000
+
+# estimates of t_c for each system size. Needs len(t_c) == len(system_size2e)
+t_c = [100, 200, 400, 1500, 5000, 18000, 70000]
+system_size2e = [4, 8, 16, 32, 64, 128, 256]
+average_heights = []
+average_square_heights = []
+
+# the 1 and 2 suffixes are for average_heights and average_square_heights respectively
+for sys_size, crit_time in tqdm(zip(system_size2e, t_c), total=len(system_size2e)):
+    model = OsloModel(sys_size)
+    sum1 = 0
+    sum2 = 0
+    for _ in range(crit_time):
+        model.run()
+    for t in range(T):
+        model.run()
+        sum1 += model.heights[0]
+        sum2 += model.heights[0] ** 2
+    result1 = sum1/T
+    result2 = sum2/T
+    average_heights.append(result1)
+    average_square_heights.append(result2)
+
+average_heights = np.array(average_heights)
+average_square_heights = np.array(average_square_heights)
+
+std_dev = np.sqrt(average_square_heights - average_heights ** 2)
+
+print(f"The system sizes are {system_size2e}")
+print(f"The average heights are {average_heights}")
+print(f"The standard deviations are {std_dev}")
+
+plt.plot(system_size2e, std_dev, ".")
+plt.xlabel("Time t")
+plt.ylabel("$\sigma_{h}(L)$")
+plt.show()
+
+#%% task 2e continued.
+plt.figure()
+
+plt.plot(system_size2e, average_heights)
 plt.show()
